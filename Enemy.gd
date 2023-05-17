7054a35c8341c15cd7a046b51788066ace063e9d
vitordefante <joaovitordefante@hotmail.com>
2023-05-17 17:28:28 -0300

    adicionado novas funcoes

--- Enemy.gd
+++ Enemy.gd
@@ -1,9 +1,35 @@
 extends Sprite
 
+export (Color) var blue = Color("#4682b4")
+export (Color) var green =  Color("#639765")
+export (Color) var red = Color("#a65455")
+
+export (float) var speed = 0.1
 
 onready var prompt = $RichTextLabel
 onready var prompt_text = prompt.text
 
 
+func _physics_process(delta: float) -> void:
+	global_position.x -= speed
+
+
 func get_prompt() -> String:
 	return prompt_text
+
+#coleta e aplica as cores
+func set_next_character(next_character_index: int):
+	var blue_text = get_bbcode_color_tag(blue) + prompt_text.substr(0, next_character_index) + get_bbcode_end_color_tag()
+	var green_text = get_bbcode_color_tag(green) + prompt_text.substr(next_character_index, 1) + get_bbcode_end_color_tag()
+	var red_text = ""
+	if next_character_index != prompt_text.length():
+		red_text = get_bbcode_color_tag(red) + prompt_text.substr(next_character_index + 1, prompt_text.length() - next_character_index + 1) + get_bbcode_end_color_tag()
+	prompt.parse_bbcode("[center]" + blue_text + green_text + red_text + "[/center]")
+
+#funcao para retornar a string com a tag da cor a ser usada
+func get_bbcode_color_tag(color: Color) -> String:
+	return "[color=#" + color.to_html(false) + "]"
+
+#coleta o "Bbcode" contendo o texto e tambema cor inserida
+func get_bbcode_end_color_tag() -> String:
+	return "[/color]"
--- Main.gd
+++ Main.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var Enemy = preload("res://Enemy.tscn")
+
+onready var enemy_container = $EnemyContainer
+onready var spawn_container = $SpawnContainer
+onready var spawn_timer = $SpawnTimer
+
 
 # detecta naves ativas na cena
 var active_enemy = null
@@ -7,16 +13,25 @@ var active_enemy = null
 #index da letra atual que necessita ser digitada
 var current_letter_index: int = -1
 
+#spawna um inimogo antes do timeout de 3 segundos
+func _ready() -> void:
+	randomize()
+	spawn_timer.start()
+	spawn_enemy()
+
 func find_new_active_enemy(typed_character: String):
-	print(typed_character)
-	print($Enemy.get_prompt())
-	var prompt = $Enemy.get_prompt()
-	if prompt.substr(0, 1) == typed_character:
-		active_enemy = $Enemy
-		print("Novo Inimigo!")
+	for enemy in enemy_container.get_children():
+		var prompt = enemy.get_prompt()
+		var next_character = prompt.substr(0, 1)
+		if next_character == typed_character:
+			print("Novo inimigo encontrado, comeca com a letra %s " % next_character)
+			active_enemy = enemy
+			current_letter_index = 1
+			active_enemy.set_next_character(current_letter_index)
+			return
 		
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey and not event.is_pressed():
+	if event is InputEventKey and event.is_pressed() and not event.is_echo():
 		var typed_event = event as InputEventKey
 		var key_typed = PoolByteArray([typed_event.unicode]).get_string_from_utf8()
 		
@@ -27,4 +42,27 @@ func _unhandled_input(event: InputEvent) -> void:
 			var prompt = active_enemy.get_prompt()
 			var next_character = prompt.substr(current_letter_index, 1)
 			if key_typed == next_character:
-				print ("success")
+				print ("successfully typed %s" % key_typed)
+				current_letter_index += 1
+				active_enemy.set_next_character(current_letter_index)
+				if current_letter_index == prompt.length():
+					print("correto")
+					current_letter_index = -1
+					active_enemy.queue_free()
+					active_enemy = null
+			else:
+				print("Voce digitou errado, letra %s ao invez de %s" % [key_typed, next_character])
+					
+
+#spawna as naves a cada 3 segundos em um spawn aleatorio
+func _on_SpawnTimer_timeout() -> void:
+	spawn_enemy()
+	
+
+func spawn_enemy():
+	var enemy_instance = Enemy.instance()
+	var spawns = spawn_container.get_children()
+	var index = randi() % spawns.size()
+	enemy_instance.global_position = spawns[index].global_position
+	enemy_container.add_child(enemy_instance)
+	
